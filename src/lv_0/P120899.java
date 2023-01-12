260644c1057585621405f6208a70e99d514c20b3
min-jung <amy453@daum.net>
2023-01-12 20:27:41 +0900

    프로그래머스 가장 큰 수 찾기

---
+++ src/lv_0/P120899.java
@@ -0,0 +1,26 @@
+package lv_0;
+
+public class P120899 {
+
+}
+
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        int max = array[0];
+        int idx = 0;
+        
+        for(int i=1;i<array.length;i++){
+            if(max<array[i]){
+                max = array[i];
+                idx = i;
+            }           
+        }
+        
+        answer[0] = max;
+        answer[1] = idx;
+        
+        return answer;
+    }
+}

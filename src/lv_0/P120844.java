af55878bdb534fa0dafadc980b4ecd8e81439da7
min-jung <amy453@daum.net>
2023-01-12 20:48:42 +0900

    프로그래머스 배열 회전시키기

---
+++ src/lv_0/P120844.java
@@ -0,0 +1,28 @@
+package lv_0;
+
+public class P120844 {
+	public int[] solution(int[] numbers, String direction) {
+        
+        if(direction.equals("left")){
+            int tmp = numbers[0];
+            
+            for(int i=0;i<numbers.length-1;i++){
+                numbers[i] = numbers[i+1];
+            }
+            
+            numbers[numbers.length-1] = tmp;
+            
+        }else{
+            int tmp = numbers[numbers.length-1];
+            
+            for(int i=numbers.length-1;i>0;i--){
+                numbers[i] = numbers[i-1];
+            }
+            
+            numbers[0] = tmp;
+            
+        }
+        return numbers;
+    }
+
+}

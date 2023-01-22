fdafbfeb0e7a98350b1d7822fc6fa2fb2c218cc2
min-jung <amy453@daum.net>
2023-01-22 12:49:58 +0900

    최빈값 구하기 방법 추가
    기존에 시도했던 방법 중, 에러가 발생했던 것을 해결함

--- src/lv_0/P120812.java
+++ src/lv_0/P120812.java
@@ -59,13 +59,13 @@ public class P120812 {
         return answer;
     }*/
 	
-    /* 에러 발생 > 왜? ..
-     * 
-     * public int solution(int[] array) {
+    /*public int solution(int[] array) {
         int answer = 0;
         
         Map<Integer,Integer> map = new LinkedHashMap<>();
         
+        //map에 키값이 없으면 key=i, value=1을 넣는다.
+        //map에 키값이 있으면 value값 +1을 넣는다.
         for(int i : array){
             if(map.get(i)==null){
                 map.put(i,1);
@@ -74,24 +74,29 @@ public class P120812 {
             }
         }
         
+        //map을 value값을 기준으로 오름차순 정렬
         List<Map.Entry<Integer,Integer>> entryList = new LinkedList<>(map.entrySet());
         entryList.sort(Map.Entry.comparingByValue());
         
+        //key를 저장할 배열, value를 저장할 배열
         int[] key = new int[map.size()];
         int[] value = new int[map.size()];
         int i = 0;
         int len = key.length;
         
+        //key와 value를 각 배열에 저장
         for(Map.Entry<Integer,Integer> entry : entryList){
             key[i] = entry.getKey();
             value[i] = entry.getValue();
             i++;
         }
         
+        //최빈값이 여러개면 -1 반환
+        //그렇지 않으면 key값 반환
         if(len>1 && (value[len-1]==value[len-2])){
             answer = -1;
         }else{
-            answer = value[len-1];
+            answer = key[len-1];
         }
         
         return answer;

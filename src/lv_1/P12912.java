19289c3c4614f1927e725bf0f6c7a71cf7cc275a
min-jung <amy453@daum.net>
2023-01-25 13:22:52 +0900

    두 정수 사이의 합

---
+++ src/lv_1/P12912.java
@@ -0,0 +1,22 @@
+package lv_1;
+
+public class P12912 {
+	
+	public long solution(int a, int b) {
+		
+        long answer = 0;
+        
+        if(a>b) {
+        	int tmp = a;
+        	a = b;
+        	b = tmp;
+        }
+        
+        for(int i=a;i<=b;i++) {
+        	answer += i;
+        }
+        
+        return answer;
+    }
+
+}

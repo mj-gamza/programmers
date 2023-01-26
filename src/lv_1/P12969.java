5337eb6129fc5052623c6f2fae062a84f2813efd
min-jung <amy453@daum.net>
2023-01-26 15:57:36 +0900

    직사각형 별찍기

---
+++ src/lv_1/P12969.java
@@ -0,0 +1,37 @@
+package lv_1;
+
+import java.util.Scanner;
+
+public class P12969 {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        /*StringBuilder sb = new StringBuilder();
+
+        sb.append("*".repeat(a));
+        sb.append("\n");
+        sb.append(sb.toString().repeat(b-1));
+
+        System.out.println(sb.toString());*/
+        
+        /*for(int i=0;i<b;i++) {
+        	for(int j=0;j<a;j++) {
+            	System.out.print("*");
+            }
+        	System.out.println("");
+        }*/
+        
+        
+        /*StringBuilder sb = new StringBuilder();
+        String str = sb.append("*".repeat(a)).toString();
+        
+        for(int i=0;i<b;i++){
+            System.out.println(str);
+        }*/
+        
+    }
+
+}
